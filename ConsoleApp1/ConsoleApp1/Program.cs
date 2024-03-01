010f51843b159f7e75b5bbc612a9e109949b4057
Sultan Kurmanov <skurmanov12@gmail.com>
2024-03-01 18:06:35 +0500

    add task 8

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Text;
 using System.Threading;
-
+using System.Linq;
 public class Book
 {
     public string nameOfBook;
@@ -56,11 +56,66 @@ public class Book
 
 }
 
+
+
+
+
+
+
+
+
+class Player
+{
+    public string Login { get; private set; }
+    public int Level { get; private set; }
+
+    public Player(string login, int level)
+    {
+        Login = login;
+        Level = level;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
+        // task 8
+        
+        List<Player> players = new List<Player>
+        {
+            new Player("sultana", 150),
+            new Player("Sultan", 200),
+            new Player("Natlus", 164),
+            new Player("Sula", 175),
+            new Player("Sul", 284),
+            new Player("tan", 134)
+        };
 
+        List<Player> newPlayers = new List<Player>
+        {
+            new Player("abc", 155),
+            new Player("cba", 185),
+            new Player("dfa", 173),
+            new Player("fds", 157)
+        };
+
+        var filtredPlayers = players.Where(player => player.Level >= 150).OrderByDescending(player => player.Level);
+
+        foreach (var player in filtredPlayers)
+        {
+            Console.WriteLine(player.Login);
+        }
+
+        var groupPlayers = players.Union(newPlayers);
+        groupPlayers = groupPlayers.OrderBy(player => player.Level);
+
+        Console.WriteLine("_____________________");
+
+        foreach (var gPlayer in groupPlayers)
+        {
+            Console.WriteLine(gPlayer.Login);
+        }
+        
         //7) Многопоточность: Напишите программу, использующую многопоточность для выполнения некоторых операций параллельно. Например, вы можете создать несколько потоков для одновременной обработки данных.
         Console.ReadKey();
         Thread myThread = new Thread(test);
---
+++ Task8/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+
+
+
+List<Player> players = new List<Player>
+{
+    new Player("sultana", 150),
+    new Player("Sultan", 200),
+    new Player("Natlus", 164),
+    new Player("Sula", 175),
+    new Player("Sul", 284),
+    new Player("tan", 134)
+};
+
+List<Player> newPlayers = new List<Player>
+{
+    new Player("abc", 155),
+    new Player("cba", 185),
+    new Player("dfa", 173),
+    new Player("fds", 157)
+};
+
+var filtredPlayers = players.Where(player => player.Level >= 150).OrderByDescending(player => player.Level);
+
+foreach (var player in filtredPlayers)
+{
+    Console.WriteLine(player.Login);
+}
+
+var groupPlayers = players.Union(newPlayers);
+groupPlayers = groupPlayers.OrderBy(player => player.Level);
+
+Console.WriteLine("_____________________");
+
+foreach (var gPlayer in groupPlayers)
+{
+    Console.WriteLine(gPlayer.Login);
+}
+
+
+
+
+class Player
+{
+    public string Login { get; private set; }
+    public int Level { get; private set; }
+
+    public Player(string login, int level)
+    {
+        Login = login;
+        Level = level;
+    }
+}
